6e06791a999d1893ff19eb7ffe15477de2434518
ACCorbett <adam.corbett@gmail.com>
2026-02-01 12:26:36 -0500

    Delete Public directory
    Correct Upload Error

--- Public/Scan-Database.ps1
+++
@@ -1,9 +0,0 @@
-function Scan-Database {
-    [CmdletBinding()]
-    param(
-        [string]$ConnectionString,
-        [string]$InputJson
-    )
-
-    throw [System.NotImplementedException] "Scan-Database is not implemented yet. TODO: parse JSON, map schema, bulk-insert results."
-}
--- Public/Scan-Domain.ps1
+++
@@ -1,9 +0,0 @@
-function Scan-Domain {
-    [CmdletBinding()]
-    param(
-        [string]$DomainName,
-        [System.Management.Automation.PSCredential]$Credential
-    )
-
-    throw [System.NotImplementedException] "Scan-Domain is not implemented yet. TODO: enumerate domain computers, orchestrate remote Scan-Computer calls, aggregate results."
-}
